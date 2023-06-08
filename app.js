08e9008017ad8eb3f4e79eda9cd830e14e03150d
Darren <hldaz77@gmail.com>
2023-06-08 16:48:29 +0100

    updated with js

---
+++ app.js
@@ -0,0 +1,7 @@
+let username = prompt("hi user what is your name!");
+const welcomeMsg = alert("Welcome to my page" + username);
+confirm("Are you human?");
+
+function msg(){  
+    alert ("Live long and prosper");  
+   }  
